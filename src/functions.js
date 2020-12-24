721644eef19fc99dd021bf017a2c24cfc3b30a36
Denis <krollik1000000@gmail.com>
2020-12-24 16:30:24 +0200

    'commit'

--- src/functions.js
+++ src/functions.js
@@ -64,7 +64,7 @@ export function render(string, element, func, list, play, show) {
                         <div className='preview-content'>
                             <div 
                                 className='preview-content__avatar' 
-                                style={{'backgroundImage': `url(./src/img/${element.image}.svg)`}}/>
+                                style={{'backgroundImage': `url(../src/img/${element.image}.svg)`}}/>
                             <span>{element.name}</span>
                             <div onClick={() => func(list, element)}>{element.favourite 
                                     ? <img 
@@ -96,7 +96,7 @@ export function render(string, element, func, list, play, show) {
         return <li className='table' key={Math.random()}>
                         <div 
                             className='table-content__avatar' 
-                            style={{'backgroundImage': `url(src/img/${element.image}.svg)`}}/>
+                            style={{'backgroundImage': `url(../src/img/${element.image}.svg)`}}/>
                         <span className='table-content__name'>{element.name}</span>
                         <span>{element.age + ' age'}</span>
                         <span>{element.phone}</span>
